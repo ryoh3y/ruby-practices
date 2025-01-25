73347c8d354c9b783e72075fc83a95c8b800ee17
ryoheiharada <account@ryoheiharada.com>
2025-01-25 16:13:19 +0900

    Fix: 4行目以下字下げを削除

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,16 +1,15 @@
 puts "What number do you want to count?"
-count = gets # カウントしたい回数
+count = 20 # カウントしたい回数
   
-  # FizzBuzzの判定
-  (1..count.to_i).each do |x| # Rangeオブジェクトを使ってcountの数まで１ずつ繰り返す
-    if x % 3 == 0 && x % 5 == 0 # ３の倍数かつ５の倍数の場合
-      puts "FizzBuzz"
-    elsif x % 3 == 0 # ３の倍数の場合
-      puts "Fizz"
-    elsif x % 5 == 0 # ５の倍数の場合
-      puts "Buzz"
-    else
-      puts x
-    end
+# FizzBuzzの判定
+(1..count.to_i).each do |x| # Rangeオブジェクトを使ってcountの数まで１ずつ繰り返す
+  if x % 3 == 0 && x % 5 == 0 # ３の倍数かつ５の倍数の場合
+    puts "FizzBuzz"
+  elsif x % 3 == 0 # ３の倍数の場合
+    puts "Fizz"
+  elsif x % 5 == 0 # ５の倍数の場合
+    puts "Buzz"
+  else
+    puts x
   end
-  
+end
