38283101d85fbe58889fa3ce4ed03b9aaddbb1c6
ryoheiharada <account@ryoheiharada.com>
2025-01-25 16:14:14 +0900

    Fix: 不要なコメントアウトを削除

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,12 +2,12 @@ puts "What number do you want to count?"
 count = 20 # カウントしたい回数
   
 # FizzBuzzの判定
-(1..count.to_i).each do |x| # Rangeオブジェクトを使ってcountの数まで１ずつ繰り返す
-  if x % 3 == 0 && x % 5 == 0 # ３の倍数かつ５の倍数の場合
+(1..count.to_i).each do |x|
+  if x % 3 == 0 && x % 5 == 0
     puts "FizzBuzz"
-  elsif x % 3 == 0 # ３の倍数の場合
+  elsif x % 3 == 0
     puts "Fizz"
-  elsif x % 5 == 0 # ５の倍数の場合
+  elsif x % 5 == 0
     puts "Buzz"
   else
     puts x
