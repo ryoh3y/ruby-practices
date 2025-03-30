490050eb02de8bdcc4175d9bb6e371fffbef3e12
ryoheiharada <account@ryoheiharada.com>
2025-03-30 17:22:49 +0900

    Fix:メソッドを使用してpoint 変数の初期化と更新と簡略化を行なった。記述の簡略化、コードの重複を削減

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -3,28 +3,26 @@
 
 # スコアを配列に変換
 score = ARGV[0].gsub('X', '10, 0')
-shots = score.split(',').map(&:strip).map(&:to_i)
+shots = score.split(',').map(&:to_i)
 frames = shots.each_slice(2).to_a
 
 # スコア計算
-point = 0
-
-frames.each_with_index do |frame, index|
-  point +=
-    if index == 9
-      frames.slice(9..-1).flatten.sum # 10フレーム目は追加点なしで合計を計算
-    elsif frame.first == 10 # ストライク時の得点計算
-      if frames[index + 1].first == 10
-        10 + frames[index + 1].first + frames[index + 2].first
-      else
-        10 + frames[index + 1].sum
-      end
-    elsif frame.sum == 10 # スペア時の得点計算
-      frame.sum + frames[index + 1].first
+point = frames.take(10).each_with_index.sum do |frame, index|
+  if index == 9
+    frames.slice(9..).flatten.sum
+  elsif frame.first == 10 # ストライク時の計算
+    strike = frame.sum
+    bonus = frames[index + 1].first
+    if frames[index + 1].first == 10
+      strike + bonus + frames[index + 2].first
     else
-      frame.sum
+      strike + bonus + frames[index + 1].last
     end
-  break if index == 9
+  elsif frame.sum == 10 # スペア時の計算
+    frame.sum + frames[index + 1].first
+  else
+    frame.sum
+  end
 end
 
 puts point
