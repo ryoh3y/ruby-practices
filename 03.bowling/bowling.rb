05ca9542b68ba8afdba9e9b01331666ad272eb09
ryoheiharada <account@ryoheiharada.com>
2025-04-05 09:35:21 +0900

    Fix:frame.sumの重複を削除

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -8,21 +8,21 @@ frames = shots.each_slice(2).to_a
 
 # スコア計算
 point = frames.take(10).each_with_index.sum do |frame, index|
-  if index == 9
-    frames.slice(9..).flatten.sum
-  elsif frame.first == 10 # ストライク時の計算
-    strike = frame.sum
-    next_frame_first_roll = frames[index + 1].first
-    strike + next_frame_first_roll + if frames[index + 1].first == 10
-      frames[index + 2].first
-    else
-      frames[index + 1].last
-    end
-  elsif frame.sum == 10 # スペア時の計算
-    frame.sum + frames[index + 1].first
-  else
-    frame.sum
-  end
+  frame_sum = frame.sum
+  next_frame_first_roll = frames[index + 1]&.first
+  frame_sum + if index == 9
+                frames.slice(9..).flatten.sum - frame_sum
+              elsif frame.first == 10 # ストライク時の計算
+                next_frame_first_roll + if frames[index + 1]&.first == 10
+                                          frames[index + 2]&.first
+                                        else
+                                          frames[index + 1]&.last
+                                        end
+              elsif frame_sum == 10 # スペア時の計算
+                frames[index + 1]&.first
+              else
+                0
+              end
 end
 
 puts point
